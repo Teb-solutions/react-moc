f985695b78a582166f4d10b3fd70aca4e7a337c6
jishnuAVijayan012 <jishnu.vijayan@seeroo.com>
2024-06-26 10:46:21 +0530

    progress style issue fixed commit

--- src/app/main/dashboards/moc/CircularProgressbar.jsx
+++ src/app/main/dashboards/moc/CircularProgressbar.jsx
@@ -3,10 +3,12 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 /**
- * The CourseProgress component.
+ * The CircularProgress component.
+ * @param {object} props - The component props.
+ * @param {number} props.course - The target progress value.
+ * @param {string} [props.className] - Optional additional class name.
  */
-const CircularProgress = (props) => {
-  const { course, className } = props;
+const CircularProgress = ({ course, className }) => {
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
@@ -27,20 +29,50 @@ const CircularProgress = (props) => {
   }, [course]);
 
   return (
-    <div style={{ width: "100px", height: "100px", position: "relative" }}>
+    <div
+      style={{ width: "100px", height: "100px", position: "relative" }}
+      className={className}
+    >
       <CircularProgressbar
         value={progress}
-        text={`${course}% `}
         styles={buildStyles({
-          textColor: "#000",
+          textColor: "#444444",
           pathColor: "rgb(83 169 255)",
           trailColor: "#e0e0df",
-          textSize: "16px",
-          backgroundColor: "#3e98c7",
+          textSize: "20px",
           pathTransitionDuration: 0.5,
           pathTransition: "ease-in-out",
         })}
       />
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center",
+          color: "#444444",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "baseline" }}>
+          <div style={{ fontSize: "20px", fontWeight: "normal" }}>{course}</div>
+          <div
+            style={{
+              fontSize: "10px",
+              fontWeight: "normal",
+              color: "#A9A9A9",
+              marginLeft: "2px",
+            }}
+          >
+            %
+          </div>
+        </div>
+        <div
+          style={{ fontSize: "10px", fontWeight: "normal", color: "#A9A9A9" }}
+        >
+          progress
+        </div>
+      </div>
     </div>
   );
 };
