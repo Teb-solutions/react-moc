60fe0a6514040483e96142a66a3a8c3ea0f8d21e
vineethSeer00 <vineeth.pk@seeroo.com>
2024-06-28 16:43:57 +0530

    security commit

--- src/app/configs/navigationConfig.js
+++ src/app/configs/navigationConfig.js
@@ -2,17 +2,16 @@ import i18next from "i18next";
 import ar from "./navigation-i18n/ar";
 import en from "./navigation-i18n/en";
 import tr from "./navigation-i18n/tr";
+import { decryptFeature } from "../main/sign-in/tabs/featureEncryption";
 
 i18next.addResourceBundle("en", "navigation", en);
 i18next.addResourceBundle("tr", "navigation", tr);
 i18next.addResourceBundle("ar", "navigation", ar);
 
-const storedFeature = localStorage.getItem("features");
+const storedFeature = decryptFeature();
 const feature = storedFeature ? storedFeature : [];
-// console.log(feature, "888");
-/**
- * The navigationConfig object is an array of navigation items for the Fuse application.
- */ console.log(feature);
+console.log("features", feature);
+
 const navigationConfig1 = [
   {
     id: "dashboards",
@@ -262,7 +261,6 @@ const shouldIncludeItem = (item) => {
   return false;
 };
 
-// Filter navigationConfig based on the features array and default items
 const filterNavigation = (config) => {
   return config
     .map((group) => ({
---
+++ src/app/main/sign-in/tabs/featureEncryption.js
@@ -0,0 +1,20 @@
+import CryptoJS from "crypto-js";
+
+import Cookies from "js-cookie";
+export const encryptFeature = (data) => {
+  const jsonString = JSON.stringify(data);
+  const secretKey = import.meta.env.VITE_SECRET_KEY;
+  const encryptedData = CryptoJS.AES.encrypt(jsonString, secretKey).toString();
+  Cookies.set("MOC_Features", encryptedData);
+};
+
+export const decryptFeature = () => {
+  const encryptedDatas = Cookies.get("MOC_Features");
+  const secretKey = import.meta.env.VITE_SECRET_KEY;
+  if (encryptedDatas) {
+    const bytes = CryptoJS.AES.decrypt(encryptedDatas, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  } else {
+    console.error("Failed to retrieve encrypted data from cookies.");
+  }
+};
